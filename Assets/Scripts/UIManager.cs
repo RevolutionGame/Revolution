b4f67d316ba9eafc498c9dc51c522b31395bd07a
aceos2201 <tue79528@temple.edu>
2018-10-19 13:10:03 -0400

    Added Basic Main Menu and Options Screen

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+
+    public void slide() { }
+
+    public void DisableBoolAnimator(Animator anim)
+    {
+        anim.SetBool("IsDisplayed", false);
+    }
+
+    public void EnableBoolAnimator(Animator anim)
+    {
+        anim.SetBool("IsDisplayed", true);
+    }
+
+    public void StartGame(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
