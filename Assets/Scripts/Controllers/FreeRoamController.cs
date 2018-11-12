7a3024fc698c4b0956ec32a50be9f5590cf0a55a
Stephen P <stephenp@ac-bc-32-9f-e5-53.wireless.temple.edu>
2018-11-12 14:48:03 -0500

    joystick

--- Assets/Scripts/Controllers/FreeRoamController.cs
+++ Assets/Scripts/Controllers/FreeRoamController.cs
@@ -8,15 +8,23 @@ public class FreeRoamController : MonoBehaviour
     public int speed = 10;
     public int rotationSpeed = 200;
 
+    public Joystick joystick;
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        joystick = FindObjectOfType<Joystick>();
     }
 
     private void FixedUpdate()
     {
-        float translattion = Input.GetAxis("Vertical") * speed * Time.deltaTime + 0.001f;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        var horizontalInput = -1 * (Input.GetAxis("Horizontal") + joystick.Horizontal);
+        var verticalInput = Input.GetAxis("Vertical") + joystick.Vertical;
+
+
+        float translattion = verticalInput * speed * Time.deltaTime + 0.001f;
+        float rotation = horizontalInput * rotationSpeed * Time.deltaTime;
         transform.Translate(new Vector2(0, translattion));
         transform.Rotate(new Vector3(0, 0, rotation));
     }
