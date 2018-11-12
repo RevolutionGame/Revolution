528dbd09fb5a3d3d4db730cf777cd8167c38017d
Stephen P <stephenp@ac-bc-32-9f-e5-53.wireless.temple.edu>
2018-11-12 11:45:18 -0500

    api managerModels

--- Assets/Scripts/Controllers/ApiTester.cs
+++ Assets/Scripts/Controllers/ApiTester.cs
@@ -2,35 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApiTester : MonoBehaviour {
+namespace modelSpace
+{
+    public class ApiTester : MonoBehaviour
+    {
 
-    ApiManager apiManager = new ApiManager();
+        ApiManager apiManager = new ApiManager();
 
-    // Use this for initialization
-    void Start () {
+        // Use this for initialization
+        void Start()
+        {
 
-        StartCoroutine(login());
+            StartCoroutine(login());
 
-       
+
+        }
+
+
+        IEnumerator login()
+        {
+
+            string email = "cs9stephen@gmail2.com";
+            string pass = "showmetheway";
+
+            CoroutineWithData cd = new CoroutineWithData(this, apiManager.LoginUser(email, pass));
+            yield return cd.coroutine;
+            LoginData ld = (LoginData)cd.result;
+
+            Debug.Log("result is " + ld.data.name);
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
-
-
-    IEnumerator login(){
-
-        string email = "cs9stephen@gmail2.com";
-        string pass = "showmetheway";
-
-        CoroutineWithData cd = new CoroutineWithData(this, apiManager.LoginUser(email, pass));
-        yield return cd.coroutine;
-        LoginData ld = (LoginData)cd.result;
-
-        Debug.Log("result is " + ld.data.name);
-
-
-    }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
