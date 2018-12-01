5383e37b919f6ac90f78879e204efab32ee47ecc
aceos2201 <tue79528@temple.edu>
2018-12-01 18:07:56 -0500

    merging

---
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayer : Player {
+    int id;
+
+
+
+    public NetworkPlayer(int id) {
+        this.id = id;
+    }
+
+    
+    public override void SpawnShip(Ship ship)
+    {
+        this.ship = ship;
+        //ship.controller = new NullController();
+    }
+
+    public void MoveTo(float x, float y, float theta) {
+      this.ship.transform.Translate(new Vector2(x, y));
+      this.ship.transform.Rotate(new Vector3(0, 0, theta));
+    }
+}
